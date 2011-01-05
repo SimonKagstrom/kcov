75d46f9fd5e515988fc2a2960047773675c58e2b
Igor Murzov <igor@gplsoft.org>
2011-01-05 17:31:21 +0300

    cmake: Fix elfutils paths searching.

--- cmake/FindLibDwarf.cmake
+++ cmake/FindLibDwarf.cmake
@@ -16,19 +16,27 @@ if (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
   set (LibDwarf_FIND_QUIETLY TRUE)
 endif (LIBDWARF_LIBRARIES AND LIBDWARF_INCLUDE_DIRS)
 
-find_path (LIBDWARF_INCLUDE_DIRS
+find_path (DWARF_INCLUDE_DIR
     NAMES
-      libdw.h
+      dwarf.h
     PATHS
       /usr/include
-      /usr/include/elfutils
       /usr/local/include
-      /usr/local/include/elfutils
       /opt/local/include
-      /opt/local/include/elfutils
       /sw/include
-      /sw/include/elfutils
+      ENV CPATH) # PATH and INCLUDE will also work
+find_path (LIBDW_INCLUDE_DIR
+    NAMES
+      elfutils/libdw.h
+    PATHS
+      /usr/include
+      /usr/local/include
+      /opt/local/include
+      /sw/include
       ENV CPATH)
+if (DWARF_INCLUDE_DIR AND LIBDW_INCLUDE_DIR)
+    set (LIBDWARF_INCLUDE_DIRS  ${DWARF_INCLUDE_DIR} ${LIBDW_INCLUDE_DIR})
+endif (DWARF_INCLUDE_DIR AND LIBDW_INCLUDE_DIR)
 
 find_library (LIBDWARF_LIBRARIES
     NAMES
@@ -38,7 +46,7 @@ find_library (LIBDWARF_LIBRARIES
       /usr/local/lib
       /opt/local/lib
       /sw/lib
-      ENV LIBRARY_PATH
+      ENV LIBRARY_PATH   # PATH and LIB will also work
       ENV LD_LIBRARY_PATH)
 
 include (FindPackageHandleStandardArgs)
@@ -50,5 +58,5 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibDwarf DEFAULT_MSG
     LIBDWARF_LIBRARIES
     LIBDWARF_INCLUDE_DIRS)
 
-
+mark_as_advanced(LIBDW_INCLUDE_DIR DWARF_INCLUDE_DIR)
 mark_as_advanced(LIBDWARF_INCLUDE_DIRS LIBDWARF_LIBRARIES)
