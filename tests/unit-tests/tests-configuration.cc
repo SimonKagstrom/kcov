f794d38d6827a197117234b3654f239d2d4cca37
Simon Kagstrom <simon.kagstrom@gmail.com>
2012-06-23 11:19:24 +0200

    unit test: Simplify configuration unit test

--- tests/unit-tests/tests-configuration.cc
+++ tests/unit-tests/tests-configuration.cc
@@ -1,76 +1,93 @@
 #include "test.hh"
 
 #include <configuration.hh>
+#include <utils.hh>
 
 #include "../../src/configuration.cc"
+
+static bool runParse(std::string args)
+{
+	char *p = strdup(args.c_str());
+	char *cur;
+	std::list<std::string> argList;
+
+	argList.push_back(std::string(crpcut::get_start_dir()) + "/test-binary");
+
+	cur = strtok(p, " ");
+	while (cur) {
+		argList.push_back(std::string(cur));
+		cur = strtok(NULL, " ");
+	}
+	free((void *)p);
+
+	const char **argv = (const char **)malloc(argList.size() * sizeof(const char*));
+
+	int i = 0;
+	for (std::list<std::string>::iterator it = argList.begin();
+			it != argList.end();
+			it++) {
+		argv[i] = (*it).c_str();
+		i++;
+	}
+
+	return IConfiguration::getInstance().parse(argList.size(), argv);
+}
 
 TEST(configuration)
 {
 	std::string filename = std::string(crpcut::get_start_dir()) + "/test-binary";
 
-	const char *argv[] = {NULL, "/tmp/vobb", filename.c_str(), "tjena"};
 	Configuration *conf = &(Configuration &)Configuration::getInstance();
 	ASSERT_TRUE(conf);
-	bool res = conf->parse(3, argv);
+
+
+	bool res = runParse(fmt("/tmp/vobb %s tjena", filename.c_str()));
 	ASSERT_TRUE(res);
 
 	const char **a = conf->getArgv();
+	printf("XYZ: %s\n", a[0]);
 	ASSERT_TRUE(filename == a[0]);
 	ASSERT_TRUE(std::string("tjena") == a[1]);
 
 	ASSERT_TRUE(conf->getBinaryName() == "test-binary");
 	ASSERT_TRUE(conf->getBinaryPath() == std::string(crpcut::get_start_dir()) + "/");
 
-	const char *argvHelp[] = {NULL, "-h"};
-	res = conf->parse(2, argvHelp);
+	res = runParse("-h");
 	ASSERT_FALSE(res);
 
-	const char *argvSortFail[] = {NULL, "-s vbb"};
-	res = conf->parse(2, argvSortFail);
+	res = runParse("-s vbb");
 	ASSERT_FALSE(res);
-
-	const char *limits = "30,60";
-	const char *argv2[] = {NULL, "-l", limits, "/tmp/vobb", "--sort-type=p", filename.c_str()};
 
 	ASSERT_TRUE(conf->getSortType() == IConfiguration::FILENAME);
 	ASSERT_TRUE(conf->m_lowLimit == 25U);
 	ASSERT_TRUE(conf->m_highLimit == 75U);
 
-	res = conf->parse(6, argv2);
+	res = runParse(fmt("-l 30,60 /tmp/vobb --sort-type=p %s", filename.c_str()));
 	ASSERT_TRUE(res);
 
 	ASSERT_TRUE(conf->getSortType() == IConfiguration::PERCENTAGE);
 	ASSERT_TRUE(conf->m_lowLimit == 30U);
 	ASSERT_TRUE(conf->m_highLimit == 60U);
 
-	limits = "20"; argv2[2] =  limits;
-	res = conf->parse(5, argv2);
+	res = runParse(fmt("-l 20 /tmp/vobb --sort-type=p %s", filename.c_str()));
 	ASSERT_TRUE(!res);
 
-	limits = "40,50,90"; argv2[2] =  limits;
-	res = conf->parse(5, argv2);
+	res = runParse(fmt("-l 40,50,90 /tmp/vobb --sort-type=p %s", filename.c_str()));
 	ASSERT_TRUE(!res);
 
-	limits = "35,hej"; argv2[2] =  limits;
-	res = conf->parse(5, argv2);
+	res = runParse(fmt("-l 35,hej /tmp/vobb --sort-type=p %s", filename.c_str()));
 	ASSERT_TRUE(!res);
 
-	limits = "yo,89"; argv2[2] =  limits;
-	res = conf->parse(5, argv2);
+	res = runParse(fmt("-l yo,89 /tmp/vobb --sort-type=p %s", filename.c_str()));
 	ASSERT_TRUE(!res);
 
-	const char *stripLevel[] = {NULL, "--path-strip-level=3", "/tmp/vobb", filename.c_str()};
-	const char *stripLevelFail[] = {NULL, "--path-strip-level=ejNummer", "/tmp/vobb", filename.c_str()};
-
-	res = conf->parse(4, stripLevelFail);
+	res = runParse(fmt("--path-strip-level=ejNummer /tmp/vobb, %s", filename.c_str()));
 	ASSERT_FALSE(res);
 
-	res = conf->parse(4, stripLevel);
+	res = runParse(fmt("--path-strip-level=3 /tmp/vobb, %s", filename.c_str()));
 	ASSERT_TRUE(res);
 
-	const char *argvPattern[] = {NULL, "--include-path=/a,/b/c", "--exclude-pattern=d/e/f", "/tmp/vobb", filename.c_str()};
-
-	res = conf->parse(5, argvPattern);
+	res = runParse(fmt("--include-path=/a,/b/c --exclude-pattern=d/e/f /tmp/vobb %s", filename.c_str()));
 	ASSERT_TRUE(res);
 
 	ASSERT_TRUE(conf->getOnlyIncludePath().size() == 2U);
