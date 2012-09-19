094d342e34054f90f7016c8c9f5c277ceb72a573
Simon Kagstrom <simon.kagstrom@gmail.com>
2012-09-19 08:35:28 +0200

    profiler: Add (unfinished) profiler implementation
    Uses the kcov html writer for producing HTML output but a special
    collector for just reading addresses from a file instead.

---
+++ src/profiler-collector.cc
@@ -0,0 +1,68 @@
+#include <collector.hh>
+#include <elf.hh>
+#include <utils.hh>
+
+#include <unordered_map>
+#include <string>
+#include <list>
+
+using namespace kcov;
+
+class Collector : public ICollector
+{
+public:
+	Collector(IElf &elf) : m_elf(elf)
+	{
+	}
+
+	void registerListener(ICollector::IListener &listener)
+	{
+		m_listeners.push_back(&listener);
+	}
+
+	int run()
+	{
+		size_t sz;
+		uint32_t *data;
+
+		data = (uint32_t *)read_file(&sz, "/tmp/addresses");
+		if (!data) {
+			error("Can't read address file\n");
+			return 1;
+		}
+
+		// This will read all addresses -> lines
+		m_elf.parse();
+
+		// FIXME: Here are assumptions that won't hold...
+		unsigned addrs = sz / sizeof(uint32_t);
+		for (unsigned i = 0; i < addrs; i++) {
+			uint32_t addr = data[i];
+
+			// Report addresses
+			for (ListenerList_t::iterator it = m_listeners.begin();
+					it != m_listeners.end();
+					it++)
+				(*it)->onAddress(addr);
+		}
+
+		free(data);
+
+		return 0;
+	}
+
+	virtual void stop()
+	{
+	}
+
+private:
+	typedef std::list<ICollector::IListener *> ListenerList_t;
+
+	IElf &m_elf;
+	ListenerList_t m_listeners;
+};
+
+ICollector &ICollector::create(IElf *elf)
+{
+	return *new Collector(*elf);
+}
