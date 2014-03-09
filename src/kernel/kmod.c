47c6ac11a7edf2e353f13fdc8963af58ab8ae53e
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-03-09 13:42:08 +0100

    kprobe-coverage: Mr Gorbachev, refactor this code!
    Untested though.

--- src/kernel/kmod.c
+++ src/kernel/kmod.c
@@ -12,6 +12,7 @@
 #include <linux/module.h>
 #include <linux/kprobes.h>
 #include <linux/list.h>
+#include <linux/mutex.h>
 #include <linux/debugfs.h>
 #include <linux/workqueue.h>
 #include <linux/wait.h>
@@ -19,6 +20,7 @@
 #include <linux/fs.h>
 #include <linux/vmalloc.h>
 #include <linux/uaccess.h>
+#include <linux/crc32.h>
 
 struct kprobe_coverage
 {
@@ -27,28 +29,47 @@ struct kprobe_coverage
 	wait_queue_head_t wq;
 
 	/* Each coverage entry is on exactly one of these lists */
-	struct list_head pending_list;
-	struct list_head hit_list;
-	spinlock_t list_lock;
+	struct list_head deferred_list; /* Probes for not-yet-loaded-modules */
+
+	struct list_head pending_list;  /* Probes which has not yet triggered */
+	struct list_head hit_list;      /* Triggered probes awaiting readout */
+
+	spinlock_t pending_hit_lock;
+	struct mutex deferred_lock;
 };
 
 struct kprobe_coverage_entry
 {
-	struct kprobe_coverage *parent;
-	unsigned long base_addr;
-	const char *module_name;
-
 	struct kprobe kp;
-	struct work_struct work;
+	u32 module_checksum; /* 0 for the kernel, otherwise the module name crc32 */
+	unsigned long base_addr;
 
 	struct list_head lh;
+	struct work_struct work;
 };
+
+struct kprobe_coverage_module
+{
+	const char *module_name; /* NULL for the kernel itself */
+};
+
+static struct kprobe_coverage *global_kpc;
+
+
+static u32 get_name_checksum(const char *name)
+{
+	/* for vmlinux */
+	if (name == NULL)
+		return 0;
+
+	return crc32(0, name, strlen(name));
+}
 
 
 static int kpc_pre_handler(struct kprobe *kp, struct pt_regs *regs)
 {
 	struct kprobe_coverage_entry *entry = (struct kprobe_coverage_entry *)
-			container_of(kp, struct kprobe_coverage_entry, kp);
+					container_of(kp, struct kprobe_coverage_entry, kp);
 
 	/* Schedule it for removal */
 	schedule_work(&entry->work);
@@ -59,15 +80,17 @@ static int kpc_pre_handler(struct kprobe *kp, struct pt_regs *regs)
 static void kpc_probe_work(struct work_struct *work)
 {
 	struct kprobe_coverage_entry *entry = (struct kprobe_coverage_entry *)
-			container_of(work, struct kprobe_coverage_entry, work);
-	struct kprobe_coverage *kpc = entry->parent;
+					container_of(work, struct kprobe_coverage_entry, work);
+	struct kprobe_coverage *kpc = global_kpc;
 
 	/* Move from pending list to the hit list */
-	spin_lock(&kpc->list_lock);
+	spin_lock(&kpc->pending_hit_lock);
+
+	unregister_kprobe(&entry->kp);
 	list_del(&entry->lh);
 	list_add_tail(&entry->lh, &kpc->hit_list);
-	unregister_kprobe(&entry->kp);
-	spin_unlock(&kpc->list_lock);
+
+	spin_unlock(&kpc->pending_hit_lock);
 
 	/* Wake up the listener */
 	wake_up(&kpc->wq);
@@ -76,92 +99,98 @@ static void kpc_probe_work(struct work_struct *work)
 
 static void kpc_free_entry(struct kprobe_coverage_entry *entry)
 {
-	vfree(entry->module_name);
 	vfree(entry);
 }
 
-static void kpc_new_entry(struct kprobe_coverage *kpc, const char *module_name,
-		unsigned long base_addr, unsigned long where)
+static struct kprobe_coverage_entry *kpc_new_entry(struct kprobe_coverage *kpc,
+		const char *module_name, struct module *mod, unsigned long where)
 {
 	struct kprobe_coverage_entry *out;
-	char *name;
-	int err;
+	unsigned long base_addr = 0;
 
-	name = vmalloc(strlen(module_name) + 1);
-	if (!name)
-		return;
+	out = vzalloc(sizeof(*out));
+	if (!out)
+		return NULL;
 
-	out = vmalloc(sizeof(*out));
-	if (!out) {
-		vfree(name);
-		return;
-	}
-	memset(out, 0, sizeof(*out));
-	strcpy(name, module_name);
-	name[strlen(module_name)] = '\0';
+	/* Setup the base address if we know it, otherwise keep at 0 */
+	if (mod)
+		base_addr = (unsigned long)mod->module_core;
 
-	out->module_name = name;
+	out->module_checksum = get_name_checksum(module_name);
 	out->base_addr = base_addr;
-	out->parent = kpc;
 	out->kp.addr = (void *)(base_addr + where);
 	out->kp.pre_handler = kpc_pre_handler;
 	INIT_WORK(&out->work, kpc_probe_work);
 
-	if ( (err = register_kprobe(&out->kp)) < 0)
-		goto err;
-
-	spin_lock(&kpc->list_lock);
-	list_add(&out->lh, &kpc->pending_list);
-	spin_unlock(&kpc->list_lock);
-
-	if (enable_kprobe(&out->kp) < 0) {
-		unregister_kprobe(&out->kp);
-		goto err;
-	}
-
-	return;
-err:
-	kpc_free_entry(out);
+	return out;
 }
 
-static void kpc_add_probe(struct kprobe_coverage *kpc,
-		const char *module_name, unsigned long where)
+static int kpc_enable_probe(struct kprobe_coverage *kpc,
+		struct kprobe_coverage_entry *entry)
 {
-	static char cur_module[MODULE_NAME_LEN];
-	static unsigned long cur_base_addr;
+	int err;
 
-	/* Cache the module data to avoid having to iterate through all
-	 * the modules each time.
-	 */
-	if (module_name == NULL) {
-		/* The kernel itself */
-		strcpy(cur_module, "");
-		cur_base_addr = 0;
+	if ( (err = register_kprobe(&entry->kp)) < 0)
+		return -EINVAL;
+
+	spin_lock(&kpc->pending_hit_lock);
+	list_add(&entry->lh, &kpc->pending_list);
+	spin_unlock(&kpc->pending_hit_lock);
+
+	if (enable_kprobe(&entry->kp) < 0) {
+		unregister_kprobe(&entry->kp);
+
+		return -EINVAL;
 	}
-	else if (strcmp(cur_module, module_name) != 0) {
-		struct module *module;
 
+	return 0;
+}
+
+static void kpc_defer_probe(struct kprobe_coverage *kpc,
+		struct kprobe_coverage_entry *entry)
+{
+	mutex_lock(&kpc->deferred_lock);
+	list_add(&entry->lh, &kpc->deferred_list);
+	mutex_unlock(&kpc->deferred_lock);
+}
+
+static void kpc_add_probe(struct kprobe_coverage *kpc, const char *module_name,
+		unsigned long where)
+{
+	struct module *module = NULL;
+	struct kprobe_coverage_entry *entry;
+	int rv = 0;
+
+	/* Lookup the module which should be instrumented */
+	if (module_name) {
 		preempt_disable();
 		module = find_module(module_name);
-		if (module) {
-			strncpy(cur_module, module->name, MODULE_NAME_LEN);
-			cur_base_addr = (unsigned long)module->module_core;
-		}
 		preempt_enable();
-		if (!module)
-			return;
 	}
 
-	/* If we are unlucky we might end up with stale data here if the
-	 * module has been removed. However, it just means that the kprobe
-	 * insertion will fail or that we instrument the wrong module, i.e.,
-	 * generating confusing but harmless data.
+	entry = kpc_new_entry(kpc, module_name, module, where);
+
+	/* Three cases:
+	 *
+	 * 1. pending module - module_name is !NULL, module is NULL: Defer
+	 *    instrumentation
+	 *
+	 * 2. vmlinux - module_name and module is NULL: Instrument directly
+	 *
+	 * 3. loaded module - module_name and module is !NULL: Instrument directly
 	 */
-	kpc_new_entry(kpc, cur_module, cur_base_addr, where);
+	if (module_name && module == NULL)
+		rv = kpc_enable_probe(kpc, entry);
+	else
+		kpc_defer_probe(kpc, entry);
+
+	/* Probe enabling might fail, just free the entry */
+	if (rv < 0)
+		kpc_free_entry(entry);
 }
 
 static void kpc_clear_list(struct kprobe_coverage *kpc,
-		struct list_head *list)
+		struct list_head *list, int do_unregister)
 {
 	struct list_head *iter;
 
@@ -170,22 +199,32 @@ static void kpc_clear_list(struct kprobe_coverage *kpc,
 
 		entry = (struct kprobe_coverage_entry *)container_of(iter,
 				struct kprobe_coverage_entry, lh);
-		unregister_kprobe(&entry->kp);
+		if (do_unregister)
+			unregister_kprobe(&entry->kp);
 		kpc_free_entry(entry);
 	}
 }
 
 static void kpc_clear(struct kprobe_coverage *kpc)
 {
-	/* Free everything on the two lists */
-	spin_lock(&kpc->list_lock);
+	/* Free everything on the lists */
+	mutex_lock(&kpc->deferred_lock);
 
-	kpc_clear_list(kpc, &kpc->hit_list);
-	kpc_clear_list(kpc, &kpc->pending_list);
+	kpc_clear_list(kpc, &kpc->deferred_list, 0);
+	INIT_LIST_HEAD(&kpc->deferred_list);
+
+	mutex_unlock(&kpc->deferred_lock);
+
+
+	spin_lock(&kpc->pending_hit_lock);
+
+	kpc_clear_list(kpc, &kpc->hit_list, 1);
+	kpc_clear_list(kpc, &kpc->pending_list, 1);
 
 	INIT_LIST_HEAD(&kpc->pending_list);
 	INIT_LIST_HEAD(&kpc->hit_list);
-	spin_unlock(&kpc->list_lock);
+
+	spin_unlock(&kpc->pending_hit_lock);
 }
 
 static void *kpc_unlink_next(struct kprobe_coverage *kpc)
@@ -197,10 +236,10 @@ static void *kpc_unlink_next(struct kprobe_coverage *kpc)
 	err = wait_event_interruptible(kpc->wq, !list_empty(&kpc->hit_list));
 	if (err < 0)
 		return NULL;
-	spin_lock(&kpc->list_lock);
+	spin_lock(&kpc->pending_hit_lock);
 	entry = list_first_entry(&kpc->hit_list, struct kprobe_coverage_entry, lh);
 	list_del(&entry->lh);
-	spin_unlock(&kpc->list_lock);
+	spin_unlock(&kpc->pending_hit_lock);
 
 	return entry;
 }
@@ -226,29 +265,26 @@ static ssize_t kpc_show_read(struct file *file, char __user *user_buf,
 	struct kprobe_coverage *kpc = file->private_data;
 	struct kprobe_coverage_entry *entry;
 	unsigned long addr;
-	char *buf;
-	size_t buf_size;
-	int n = -ENOMEM;
+	char buf[64];
+	int n;
 
 	/* Wait for an entry */
 	entry = kpc_unlink_next(kpc);
 	if (!entry)
 		return 0;
 
-	buf_size = strlen(entry->module_name) + 24;
-	buf = vmalloc(buf_size);
-	if (!buf)
-		goto out;
-
 	/* Write it out to the user buffer */
-	memset(buf, 0, buf_size);
 	addr = (unsigned long)entry->kp.addr - entry->base_addr;
-	n = snprintf(buf, buf_size, "%s:0x%lx\n", entry->module_name, addr);
+
+	n = snprintf(buf, sizeof(buf), "0x%08x:0x%16lx\n",
+			entry->module_checksum, addr);
+	/* Should never happen if snprintf works correctly */
+	if (n >= sizeof(buf))
+		return -EINVAL;
+
 	if (copy_to_user(user_buf + *off, buf, n))
 		n = -EFAULT;
 
-out:
-	vfree(buf);
 	kpc_free_entry(entry);
 
 	return n;
@@ -312,6 +348,68 @@ static ssize_t kpc_control_write(struct file *file, const char __user *user_buf,
 	return out;
 }
 
+static void kpc_handle_coming_module(struct kprobe_coverage *kpc,
+		struct module *mod)
+{
+	struct list_head *iter;
+	struct list_head *tmp;
+
+	mutex_lock(&kpc->deferred_lock);
+	list_for_each_safe(iter, tmp, &kpc->deferred_list) {
+		struct kprobe_coverage_entry *entry;
+
+		entry = (struct kprobe_coverage_entry *)container_of(iter,
+				struct kprobe_coverage_entry, lh);
+
+		if (get_name_checksum(mod->name) != entry->module_checksum)
+			continue;
+
+		/* Move the deferred entry to the pending list and enable */
+		list_del(&entry->lh);
+
+		if (kpc_enable_probe(kpc, entry) < 0)
+			kpc_free_entry(entry);
+	}
+	mutex_unlock(&kpc->deferred_lock);
+}
+
+static void kpc_handle_going_module(struct kprobe_coverage *kpc,
+		struct module *mod)
+{
+	struct list_head *iter;
+	struct list_head *tmp;
+
+	spin_lock(&kpc->pending_hit_lock);
+	list_for_each_safe(iter, tmp, &kpc->pending_list) {
+		struct kprobe_coverage_entry *entry;
+
+		entry = (struct kprobe_coverage_entry *)container_of(iter,
+				struct kprobe_coverage_entry, lh);
+
+		if (get_name_checksum(mod->name) != entry->module_checksum)
+			continue;
+
+		/* Remove pending entries for the current module */
+		unregister_kprobe(&entry->kp);
+		list_del(&entry->lh);
+	}
+	spin_unlock(&kpc->pending_hit_lock);
+}
+
+static int kpc_module_notifier(struct notifier_block *nb,
+		unsigned long event, void *data)
+{
+	struct module *mod = data;
+
+	if (event == MODULE_STATE_COMING)
+		kpc_handle_coming_module(global_kpc, mod);
+	else if (event == MODULE_STATE_GOING)
+		kpc_handle_going_module(global_kpc, mod);
+
+	return 0;
+}
+
+
 static const struct file_operations kpc_control_fops =
 {
 	.owner = THIS_MODULE,
@@ -324,6 +422,11 @@ static const struct file_operations kpc_show_fops =
 	.owner = THIS_MODULE,
 	.open = kpc_show_open,
 	.read = kpc_show_read,
+};
+
+static struct notifier_block kpc_module_notifier_block =
+{
+	.notifier_call = kpc_module_notifier,
 };
 
 static int __init kpc_init(struct kprobe_coverage *kpc)
@@ -342,20 +445,25 @@ static int __init kpc_init(struct kprobe_coverage *kpc)
 			&kpc_show_fops))
 		goto out_files;
 
+	if (register_module_notifier(&kpc_module_notifier_block) < 0)
+		goto out_files;
+
 	INIT_LIST_HEAD(&kpc->pending_list);
 	INIT_LIST_HEAD(&kpc->hit_list);
+	INIT_LIST_HEAD(&kpc->deferred_list);
+
 	init_waitqueue_head(&kpc->wq);
-	spin_lock_init(&kpc->list_lock);
+	spin_lock_init(&kpc->pending_hit_lock);
+	mutex_init(&kpc->deferred_lock);
 
 	return 0;
 
-out_files:
+	out_files:
 	debugfs_remove_recursive(kpc->debugfs_root);
 
 	return -EINVAL;
 }
 
-static struct kprobe_coverage *global_kpc;
 static int __init kpc_init_module(void)
 {
 	int out;
@@ -376,6 +484,8 @@ static void __exit kpc_exit_module(void)
 {
 	debugfs_remove_recursive(global_kpc->debugfs_root);
 	kpc_clear(global_kpc);
+
+	unregister_module_notifier(&kpc_module_notifier_block);
 
 	vfree(global_kpc);
 	global_kpc = NULL;
