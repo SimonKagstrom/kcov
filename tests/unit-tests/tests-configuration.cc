c3a89e4cccd0e8544e38224190721f9ceceff100
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-04-16 19:24:33 +0200

    unit test: configuration: Indent and put in testsuite

--- tests/unit-tests/tests-configuration.cc
+++ tests/unit-tests/tests-configuration.cc
@@ -34,96 +34,99 @@ static bool runParse(std::string args)
 	return IConfiguration::getInstance().parse(argList.size(), argv);
 }
 
-TEST(configuration)
+TESTSUITE(configuration)
 {
-	std::string filename = std::string(crpcut::get_start_dir()) + "/test-binary";
+	TEST(basic)
+	{
+		std::string filename = std::string(crpcut::get_start_dir()) + "/test-binary";
 
-	Configuration *conf = &(Configuration &)Configuration::getInstance();
-	ASSERT_TRUE(conf);
+		Configuration *conf = &(Configuration &)Configuration::getInstance();
+		ASSERT_TRUE(conf);
 
 
-	bool res = runParse(fmt("/tmp/vobb %s tjena", filename.c_str()));
-	ASSERT_TRUE(res);
+		bool res = runParse(fmt("/tmp/vobb %s tjena", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	const char **a = conf->getArgv();
-	ASSERT_TRUE(filename == a[0]);
-	ASSERT_TRUE(std::string("tjena") == a[1]);
+		const char **a = conf->getArgv();
+		ASSERT_TRUE(filename == a[0]);
+		ASSERT_TRUE(std::string("tjena") == a[1]);
 
-	ASSERT_TRUE(conf->getBinaryName() == "test-binary");
-	ASSERT_TRUE(conf->getBinaryPath() == std::string(crpcut::get_start_dir()) + "/");
+		ASSERT_TRUE(conf->getBinaryName() == "test-binary");
+		ASSERT_TRUE(conf->getBinaryPath() == std::string(crpcut::get_start_dir()) + "/");
 
-	res = runParse("-h");
-	ASSERT_FALSE(res);
+		res = runParse("-h");
+		ASSERT_FALSE(res);
 
-	res = runParse("-s vbb");
-	ASSERT_FALSE(res);
+		res = runParse("-s vbb");
+		ASSERT_FALSE(res);
 
-	ASSERT_TRUE(conf->getSortType() == IConfiguration::FILENAME);
-	ASSERT_TRUE(conf->m_lowLimit == 25U);
-	ASSERT_TRUE(conf->m_highLimit == 75U);
+		ASSERT_TRUE(conf->getSortType() == IConfiguration::FILENAME);
+		ASSERT_TRUE(conf->m_lowLimit == 25U);
+		ASSERT_TRUE(conf->m_highLimit == 75U);
 
-	res = runParse(fmt("-l 30,60 /tmp/vobb --sort-type=p %s", filename.c_str()));
-	ASSERT_TRUE(res);
+		res = runParse(fmt("-l 30,60 /tmp/vobb --sort-type=p %s", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	ASSERT_TRUE(conf->getSortType() == IConfiguration::PERCENTAGE);
-	ASSERT_TRUE(conf->m_lowLimit == 30U);
-	ASSERT_TRUE(conf->m_highLimit == 60U);
+		ASSERT_TRUE(conf->getSortType() == IConfiguration::PERCENTAGE);
+		ASSERT_TRUE(conf->m_lowLimit == 30U);
+		ASSERT_TRUE(conf->m_highLimit == 60U);
 
-	res = runParse(fmt("-l 20 /tmp/vobb --sort-type=p %s", filename.c_str()));
-	ASSERT_TRUE(!res);
+		res = runParse(fmt("-l 20 /tmp/vobb --sort-type=p %s", filename.c_str()));
+		ASSERT_TRUE(!res);
 
-	res = runParse(fmt("-l 40,50,90 /tmp/vobb --sort-type=p %s", filename.c_str()));
-	ASSERT_TRUE(!res);
+		res = runParse(fmt("-l 40,50,90 /tmp/vobb --sort-type=p %s", filename.c_str()));
+		ASSERT_TRUE(!res);
 
-	res = runParse(fmt("-l 35,hej /tmp/vobb --sort-type=p %s", filename.c_str()));
-	ASSERT_TRUE(!res);
+		res = runParse(fmt("-l 35,hej /tmp/vobb --sort-type=p %s", filename.c_str()));
+		ASSERT_TRUE(!res);
 
-	res = runParse(fmt("-l yo,89 /tmp/vobb --sort-type=p %s", filename.c_str()));
-	ASSERT_TRUE(!res);
+		res = runParse(fmt("-l yo,89 /tmp/vobb --sort-type=p %s", filename.c_str()));
+		ASSERT_TRUE(!res);
 
-	res = runParse(fmt("/tmp/vobb --sort-type=u %s", filename.c_str()));
-	ASSERT_TRUE(res);
-	ASSERT_TRUE(conf->getSortType() == IConfiguration::UNCOVERED_LINES);
+		res = runParse(fmt("/tmp/vobb --sort-type=u %s", filename.c_str()));
+		ASSERT_TRUE(res);
+		ASSERT_TRUE(conf->getSortType() == IConfiguration::UNCOVERED_LINES);
 
-	res = runParse(fmt("/tmp/vobb --sort-type=l %s", filename.c_str()));
-	ASSERT_TRUE(res);
-	ASSERT_TRUE(conf->getSortType() == IConfiguration::FILE_LENGTH);
+		res = runParse(fmt("/tmp/vobb --sort-type=l %s", filename.c_str()));
+		ASSERT_TRUE(res);
+		ASSERT_TRUE(conf->getSortType() == IConfiguration::FILE_LENGTH);
 
-	res = runParse(fmt("--path-strip-level=ejNummer /tmp/vobb, %s", filename.c_str()));
-	ASSERT_FALSE(res);
+		res = runParse(fmt("--path-strip-level=ejNummer /tmp/vobb, %s", filename.c_str()));
+		ASSERT_FALSE(res);
 
-	res = runParse(fmt("--path-strip-level=3 /tmp/vobb, %s", filename.c_str()));
-	ASSERT_TRUE(res);
+		res = runParse(fmt("--path-strip-level=3 /tmp/vobb, %s", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	res = runParse(fmt("--include-path=/a,/b/c --exclude-pattern=d/e/f /tmp/vobb %s", filename.c_str()));
-	ASSERT_TRUE(res);
+		res = runParse(fmt("--include-path=/a,/b/c --exclude-pattern=d/e/f /tmp/vobb %s", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	ASSERT_TRUE(conf->getOnlyIncludePath().size() == 2U);
-	ASSERT_TRUE(conf->getOnlyIncludePath()[0] == "/a");
-	ASSERT_TRUE(conf->getOnlyIncludePath()[1] == "/b/c");
+		ASSERT_TRUE(conf->getOnlyIncludePath().size() == 2U);
+		ASSERT_TRUE(conf->getOnlyIncludePath()[0] == "/a");
+		ASSERT_TRUE(conf->getOnlyIncludePath()[1] == "/b/c");
 
-	ASSERT_TRUE(conf->getExcludePattern().size() == 1U);
-	ASSERT_TRUE(conf->getExcludePattern()[0] == "d/e/f");
+		ASSERT_TRUE(conf->getExcludePattern().size() == 1U);
+		ASSERT_TRUE(conf->getExcludePattern()[0] == "d/e/f");
 
-	res = runParse(fmt("--include-path=~/a /tmp/vobb %s", filename.c_str()));
-	ASSERT_TRUE(res);
+		res = runParse(fmt("--include-path=~/a /tmp/vobb %s", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	ASSERT_TRUE(conf->getOnlyIncludePath().size() == 1U);
-	ASSERT_TRUE(conf->getOnlyIncludePath()[0] == fmt("%s/a", get_home()));
+		ASSERT_TRUE(conf->getOnlyIncludePath().size() == 1U);
+		ASSERT_TRUE(conf->getOnlyIncludePath()[0] == fmt("%s/a", get_home()));
 
-	ASSERT_TRUE(conf->getAttachPid() == 0U);
-	res = runParse(fmt("-p ejNummer /tmp/vobb %s", filename.c_str()));
-	ASSERT_FALSE(res);
+		ASSERT_TRUE(conf->getAttachPid() == 0U);
+		res = runParse(fmt("-p ejNummer /tmp/vobb %s", filename.c_str()));
+		ASSERT_FALSE(res);
 
-	res = runParse(fmt("-p 10 /tmp/vobb %s", filename.c_str()));
-	ASSERT_TRUE(res);
+		res = runParse(fmt("-p 10 /tmp/vobb %s", filename.c_str()));
+		ASSERT_TRUE(res);
 
-	res = runParse(fmt("--pid=100 /tmp/vobb %s", filename.c_str()));
-	ASSERT_TRUE(res);
-	ASSERT_TRUE(conf->getAttachPid() == 100U);
+		res = runParse(fmt("--pid=100 /tmp/vobb %s", filename.c_str()));
+		ASSERT_TRUE(res);
+		ASSERT_TRUE(conf->getAttachPid() == 100U);
 
-	ASSERT_TRUE(g_kcov_debug_mask == STATUS_MSG);
-	res = runParse(fmt("--debug=7 /tmp/vobb %s", filename.c_str()));
-	ASSERT_TRUE(res);
-	ASSERT_TRUE(g_kcov_debug_mask == 7);
+		ASSERT_TRUE(g_kcov_debug_mask == STATUS_MSG);
+		res = runParse(fmt("--debug=7 /tmp/vobb %s", filename.c_str()));
+		ASSERT_TRUE(res);
+		ASSERT_TRUE(g_kcov_debug_mask == 7);
+		}
 }
