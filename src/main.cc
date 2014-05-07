9bededf29ab3d3b666a92fb2c9e13e8e55da26a2
Simon Kagstrom <simon.kagstrom@netinsight.net>
2014-05-07 10:03:46 +0200

    main: Create merged cobertura writer with the correct reporter/parser

--- src/main.cc
+++ src/main.cc
@@ -134,7 +134,7 @@ int main(int argc, const char *argv[])
 		IReporter &mergeReporter = IReporter::create(mergeParser, mergeParser);
 		IWriter &mergeHtmlWriter = createHtmlWriter(mergeParser, mergeReporter,
 				base, base + "/kcov-merged", "[merged]", false);
-		IWriter &mergeCoberturaWriter = createCoberturaWriter(*parser, reporter,
+		IWriter &mergeCoberturaWriter = createCoberturaWriter(mergeParser, mergeReporter,
 				base + "kcov-merged/cobertura.xml");
 		mkdir(fmt("%s/kcov-merged", base.c_str()).c_str(), 0755);
 
