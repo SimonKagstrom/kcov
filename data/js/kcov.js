fcf8db1d5640eb0a3d02b83ff986ff09da6e3b1f
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-05-28 13:54:18 +0200

    html-writer: Begin refactoring to generating json data
    Use template HTML files and generate JSON data instead.

---
+++ data/js/kcov.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+	Tempo.prepare('source-code').render(data);
+
+	elem = document.getElementById('header-percent-covered')
+
+	elem.className = toCoverPercentString(header.covered, header.instrumented);
+	elem.innerHTML = ((header.covered / header.instrumented) * 100).toFixed(1) + "%";	    
+
+	document.getElementById('header-command').innerHTML = header.command;
+	document.getElementById('header-date').innerHTML = header.date;
+	document.getElementById('header-covered').innerHTML = header.covered
+	document.getElementById('header-instrumented').innerHTML = header.instrumented
+}
+
+function toCoverPercentString (covered, instrumented) {
+	perc = (covered / instrumented) * 100;
+
+	if (perc <= percent_low)
+		return "coverPerLeftLo";
+	else if (perc >= percent_high)
+		return "coverPerLeftHi";
+
+	return "coverPerLeftMed";
+}
