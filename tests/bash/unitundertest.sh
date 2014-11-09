3c17acf84ca8bd0172c5bb2051c2c484df918084
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-11-09 10:42:47 +0100

    Issue #37: robot-framework: Add regression test for bash accumulation

---
+++ tests/bash/unitundertest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#unitundertest.sh
+
+function block1()
+{
+    echo "In block1()"
+
+    for f in 1 2 3 4 5
+    do
+        echo $f
+    done
+}
+
+function block2()
+{
+    echo "In block2()"
+
+    for f in 1 2 3 4 5
+    do
+        echo $f
+    done
+}
+
+#
+# Main
+#
+
+case $1 in
+1)
+    block1
+    ;;
+2)
+    block2
+    ;;
+all)
+    block1
+    block2
+    ;;
+esac
+exit 0
