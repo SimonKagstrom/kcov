c9d529a969c627968fe519569a87d69fb4133f3a
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-11-21 19:19:26 +0100

    Issue #39: robot-framework: Add simple test for html tie-breakers

--- tests/bash/first-dir/a.sh
+++ tests/bash/first-dir/a.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 echo A first
+
+if [ $1 = "hej" ] ; then
+	source `dirname $0`/c.sh
+fi
+
 source `dirname $0`/b.sh
 
--- tests/bash/second-dir/a.sh
+++ tests/bash/second-dir/a.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 echo A second
+
+if [ $1 = "hej" ] ; then
+	source `dirname $0`/c.sh
+fi
+
 source `dirname $0`/b.sh
 
