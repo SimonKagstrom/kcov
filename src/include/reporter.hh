6adc43c8f3beb6580c884ea53d887600a4f53251
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-11-24 19:55:55 +0100

    reporter: Document parts of the class

--- src/include/reporter.hh
+++ src/include/reporter.hh
@@ -10,6 +10,11 @@ namespace kcov
 	class ICollector;
 	class IFilter;
 
+	/**
+	 * Interface class that reports addresses pairs being executed.
+	 *
+	 * Can also be queried for code lines etc.
+	 */
 	class IReporter
 	{
 	public:
@@ -42,9 +47,19 @@ namespace kcov
 			unsigned int m_includeInTotals;
 		};
 
+		/**
+		 * Listener to executed addresses.
+		 */
 		class IListener
 		{
 		public:
+			/**
+			 * An address has been executed by the covered program. Can also
+			 * report data from previous runs.
+			 *
+			 * @param addr the executed address
+			 * @param hits the number of hits of the address (typically 1)
+			 */
 			virtual void onAddress(unsigned long addr, unsigned long hits) = 0;
 		};
 
@@ -68,14 +83,37 @@ namespace kcov
 		 */
 		virtual bool fileIsIncluded(const std::string &file) = 0;
 
+		/**
+		 * Returns if a file/line pair contains executable code.
+		 *
+		 * @param file the filename
+		 * @param lineNr the line number in the file
+		 *
+		 * @return true if this is executable code, false otherwise
+		 */
 		virtual bool lineIsCode(const std::string &file, unsigned int lineNr) = 0;
 
 		virtual LineExecutionCount getLineExecutionCount(const std::string &file, unsigned int lineNr) = 0;
 
 		virtual ExecutionSummary getExecutionSummary() = 0;
 
+		/**
+		 * Marshal collected data to a data chunk.
+		 *
+		 * @param szOut the size of the data (return parameter)
+		 *
+		 * @return the data, or NULL if something failed
+		 */
 		virtual void *marshal(size_t *szOut) = 0;
 
+		/**
+		 * Unmarshal marshalled data in-place.
+		 *
+		 * @param data the data to unmarshal
+		 * @param sz the size of the data
+		 *
+		 * @return true if the data could be unmarshalled, false otherwise
+		 */
 		virtual bool unMarshal(void *data, size_t sz) = 0;
 
 		static IReporter &create(IFileParser &elf, ICollector &collector, IFilter &filter);
