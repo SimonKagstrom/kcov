0d5fb08d96571e658f3f7542badd26c426c83291
Simon Kagstrom <simon.kagstrom@gmail.com>
2014-12-04 21:22:26 +0100

    writer: Document

--- src/include/writer.hh
+++ src/include/writer.hh
@@ -5,15 +5,29 @@ namespace kcov
 	class IFileParser;
 	class IReporter;
 
+	/**
+	 * Class that writes output (HTML, Cobertura, ...)
+	 */
 	class IWriter
 	{
 	public:
 		virtual ~IWriter() {}
 
+		/**
+		 * Called once on startup to setup the writer.
+		 */
 		virtual void onStartup() = 0;
 
+		/**
+		 * Called once on termination to shutdown the writer.
+		 */
 		virtual void onStop() = 0;
 
+		/**
+		 * Write current data.
+		 *
+		 * Called in regular intervals during execution.
+		 */
 		virtual void write() = 0;
 	};
 }
