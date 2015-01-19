dcabd3bd41ea78fdce1e04dbec098f4225c8d4bf
Simon Kagstrom <simon.kagstrom@gmail.com>
2015-01-19 19:14:09 +0100

    .travis.yml
    travis: Build v23 to catch performance regressions (still just the build)

--- tests/subdir2/file2.c
+++ tests/subdir2/file2.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include "../include/header.h"
 
+int b;
 void tjoho2(int a)
 {
+	int i;
+	for (i = 0; i < 2; i++)
+		b++;
+	a++;
+
 	if (a)
 		printf("Hej\n");
+	if (a + 1 == 19)
+		printf("Svampo\n");
 }
