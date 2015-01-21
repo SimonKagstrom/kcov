d3c2cb968b17d0a36da28977a5ecdcd5114bdbe5
Simon Kagstrom <simon.kagstrom@gmail.com>
2015-01-21 20:15:12 +0100

    travis: Add script to run performance tests

---
+++ travis/perf-test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+	echo "Too few arguments"
+	exit 1
+fi
+
+KCOV=$1
+
+$KCOV /tmp/kcov-perf build/src/kcov
+$KCOV /tmp/kcov-perf tests/python/main 5
+$KCOV /tmp/kcov-perf tests/bash/shell-main 5
+$KCOV /tmp/kcov-perf build/src/kcov /tmp/kcov-perf tests/python/main 5
+$KCOV /tmp/kcov-perf build/src/kcov /tmp/kcov-perf tests/bash/shell-main 5
