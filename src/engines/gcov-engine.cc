5dec68b0e59516b4afaa126e4d82096cbfbe4882
Simon Kagstrom <simon.kagstrom@gmail.com>
2015-01-26 20:37:26 +0100

    gcov-engine: Implement (broken though) gcov engine

--- src/engines/gcov-engine.cc
+++ src/engines/gcov-engine.cc
@@ -4,15 +4,25 @@
 #include <file-parser.hh>
 #include <gcov.hh>
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+
 #include <vector>
 #include <unordered_map>
+#include <utility> // std::pair
 
 using namespace kcov;
 
 class GcovEngine : public IEngine, IFileParser::IFileListener
 {
 public:
-	GcovEngine(IFileParser &parser)
+	GcovEngine(IFileParser &parser) :
+		m_listener(NULL),
+		m_child(-1)
 	{
 		parser.registerFileListener(*this);
 	}
@@ -24,6 +34,21 @@ public:
 
 	bool start(IEventListener &listener, const std::string &executable)
 	{
+		char *const *argv = (char *const *)IConfiguration::getInstance().getArgv();
+
+		m_listener = &listener;
+
+		// Run the program until completion
+		m_child = fork();
+		if (m_child == 0) {
+			printf("Exec: %s, %s\n", executable.c_str(), argv[0]);
+			execv(argv[0], argv);
+		} else if (m_child < 0) {
+			perror("fork");
+
+			return false;
+		}
+
 		return true;
 	}
 
@@ -34,14 +59,66 @@ public:
 
 	bool continueExecution()
 	{
+		if (m_child < 0)
+			return false;
+
+		int status;
+
+		// Wait for the child
+		waitpid(m_child, &status, 0);
+
+		m_child = -1;
+
+		// All coverage collection is done after the program has been run
+		for (FileList_t::const_iterator it = m_gcdaFiles.begin();
+				it != m_gcdaFiles.end();
+				++it) {
+			const std::string &gcda = *it;
+			std::string gcno = gcda;
+
+			size_t sz = gcno.size();
+
+			// .gcda -> .gcno
+			gcno[sz - 2] = 'n';
+			gcno[sz - 1] = 'o';
+
+			// Need a pair
+			if (!file_exists(gcno) || !file_exists(gcda))
+				continue;
+
+			parseGcovFiles(gcno, gcda);
+		}
+
+		Event ev(ev_exit, WEXITSTATUS(status));
+
+		// Report the exit status
+		m_listener->onEvent(ev);
+
+		return false;
+	}
+
+private:
+	typedef std::vector<std::string> FileList_t;
+
+	void parseGcovFiles(const std::string &gcnoFile, const std::string gcdaFile)
+	{
 		size_t sz;
-		void *d = read_file(&sz, "%s", "/home/ska/projects/build/kcov/build-tests/CMakeFiles/main-tests-gcov.dir/subdir2/file2.c.gcno");
+		void *d = read_file(&sz, "%s", gcnoFile.c_str());
+
+		if (!d)
+			return;
 		GcnoParser gcno((uint8_t *)d, sz);
-		void *d2 = read_file(&sz, "%s", "/home/ska/projects/build/kcov/build-tests/CMakeFiles/main-tests-gcov.dir/subdir2/file2.c.gcda");
+
+		void *d2 = read_file(&sz, "%s", gcdaFile.c_str());
+
+		if (!d2)
+			return;
 		GcdaParser gcda((uint8_t *)d2, sz);
 
 		gcno.parse();
 		gcda.parse();
+
+		std::unordered_map<int32_t, const GcnoParser::BasicBlockMapping *> bbsByNumber;
 
 		const GcnoParser::BasicBlockList_t &bbs = gcno.getBasicBlocks();
 		const GcnoParser::ArcList_t &arcs = gcno.getArcs();
@@ -51,21 +128,35 @@ public:
 				++it) {
 			const GcnoParser::BasicBlockMapping &cur = *it;
 
-			printf("  BB %2d: %s:%3d\n", cur.m_basicBlock, cur.m_file.c_str(), cur.m_line);
+			bbsByNumber[cur.m_basicBlock] = &cur;
 		}
+
+		unsigned int n = 0;
 		for (GcnoParser::ArcList_t::const_iterator it = arcs.begin();
 				it != arcs.end();
-				++it) {
+				++it, n++) {
 			const GcnoParser::Arc &cur = *it;
 
-			printf("  Arc from %2d to %2d\n", cur.m_srcBlock, cur.m_dstBlock);
+			int64_t counter = gcda.getCounter(cur.m_function, n);
+
+			// Not found?
+			if (counter < 0) {
+				warning("Arc %u but no counter\n", n);
+				continue;
+			}
+
+			const GcnoParser::BasicBlockMapping *bb = bbsByNumber[cur.m_dstBlock];
+			if (!bb) {
+				warning("No BB for function %u\n", cur.m_dstBlock);
+				continue;
+			}
+
+			uint64_t addr = gcovGetAddress(bb->m_file, bb->m_function, bb->m_basicBlock);
+			Event ev(ev_breakpoint, counter, addr);
+
+			m_listener->onEvent(ev);
 		}
-
-		return false;
 	}
-
-private:
-	typedef std::vector<std::string> FileList_t;
 
 	// From IFileParser::IFileListener
 	void onFile(const std::string &file, enum IFileParser::FileFlags flags)
@@ -80,6 +171,8 @@ private:
 	}
 
 	FileList_t m_gcdaFiles;
+	IEventListener *m_listener;
+	pid_t m_child;
 };
 
 
