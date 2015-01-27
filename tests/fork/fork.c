ff61847f3c51ed4d32d04d19e1698bf05d9be101
Simon Kagstrom <simon.kagstrom@netinsight.net>
2015-01-27 09:38:56 +0100

    tests: Remove unused headers from fork test

--- tests/fork/fork.c
+++ tests/fork/fork.c
@@ -1,10 +1,8 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/ptrace.h>
 #include <sys/user.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sched.h>
 
 static void mibb(void)
 {
