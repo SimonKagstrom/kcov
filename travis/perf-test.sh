7d0445a47e51af24a32e6b5e5027ab93305a4c68
Simon Kagstrom <simon.kagstrom@gmail.com>
2015-02-06 19:38:19 +0100

    travis: Add src and build directory for the perf test

--- travis/perf-test.sh
+++ travis/perf-test.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-if [ $# -ne 1 ] ; then
-	echo "Too few arguments"
+if [ $# -ne 3 ] ; then
+	echo "Usage: perf-test.sh <kcov> <src-dir> <build-dir>"
 	exit 1
 fi
 
 KCOV=$1
+SRC=$2
+BUILD=$3
 
-$KCOV /tmp/kcov-perf build/src/kcov
-$KCOV /tmp/kcov-perf tests/python/main 5
-$KCOV /tmp/kcov-perf tests/bash/shell-main 5
-$KCOV /tmp/kcov-perf build/src/kcov /tmp/kcov-perf tests/python/main 5
-$KCOV /tmp/kcov-perf build/src/kcov /tmp/kcov-perf tests/bash/shell-main 5
+$KCOV /tmp/kcov-perf $BUILD/src/kcov
+$KCOV /tmp/kcov-perf $SRC/tests/python/main 5
+$KCOV /tmp/kcov-perf $SRC/tests/bash/shell-main 5
+$KCOV /tmp/kcov-perf $BUILD/src/kcov /tmp/kcov-perf $SRC/tests/python/main 5
+$KCOV /tmp/kcov-perf $BUILD/src/kcov /tmp/kcov-perf $SRC/tests/bash/shell-main 5
 
 exit 0
