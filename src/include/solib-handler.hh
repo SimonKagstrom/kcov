SimonKagstrom/kcov

commit e5a3c514c694ef236a47a2cc40a3322d2490f6d2

main: Add file after creating reporter etc (needs to be notified)

src/main.cc
@@ -158,7 +158,6 @@ int main(int argc, const char *argv[])
 		conf.printUsage();
 		return 1;
 	}
-	parser->addFile(file);
 
 	// Match and create an engine
 	IEngineFactory::IEngineCreator &engineCreator = IEngineFactory::getInstance().matchEngine(file);
@@ -175,6 +174,8 @@ int main(int argc, const char *argv[])
 	IReporter &reporter = IReporter::create(*parser, collector, filter);
 	IOutputHandler &output = IOutputHandler::create(*parser, reporter, collector);
 	ISolibHandler &solibHandler = createSolibHandler(*parser, collector);
+
+	parser->addFile(file);
 
 	IConfiguration::RunMode_t runningMode = (IConfiguration::RunMode_t)conf.keyAsInt("running-mode");
 

commit 3d537d8c0686bb5f8ab97f419997f52c2f1ab253

solib-handler/main: Startup solib handler when everything has been created
Unbreaks solib handling which broke in

  e5a3c514c694ef236a47a2cc40a3322d2490f6d2

src/include/solib-handler.hh
@@ -11,6 +11,8 @@ namespace kcov
 		virtual ~ISolibHandler()
 		{
 		}
+
+		virtual void startup() = 0;
 	};
 
 	ISolibHandler &createSolibHandler(IFileParser &parser, ICollector &collector);

src/main.cc
@@ -228,6 +228,7 @@ int main(int argc, const char *argv[])
 
 	parser->setupParser(&filter);
 	output.start();
+	solibHandler.startup();
 
 	int ret = 0;
 

src/solib-handler.cc
@@ -40,10 +40,8 @@ public:
 		memset(&m_solibThread, 0, sizeof(m_solibThread));
 
 		// Only useful for ELF binaries
-		if (parser.getParserType() == "ELF" && !IConfiguration::getInstance().keyAsInt("gcov")) {
+		if (parser.getParserType() == "ELF" && !IConfiguration::getInstance().keyAsInt("gcov"))
 			collector.registerEventTickListener(*this);
-			startup();
-		}
 
 		// Skip this very special library
 		m_foundSolibs["libkcov_sowrapper.so"] = true;