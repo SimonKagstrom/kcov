ddd215254d557f558d19b125c72d68a57ec0c285
Simon Kagstrom <simon.kagstrom@gmail.com>
2015-08-04 13:38:23 +0200

    Issue #110: robot-framework: Add regression test for vfork issue

---
+++ tests/fork/vfork.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/user.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, const char *argv[])
+{
+	pid_t child;
+	int status;
+
+	child = vfork();
+	if (child < 0) {
+		fprintf(stderr, "fork failed!\n");
+		return -1;
+	}
+
+	if (child == 0)
+		printf("Parent %d\n", getpid());
+	else {
+		execle("/bin/ls", "/bin/ls", "/proc/self/exe", NULL, NULL);
+	}
+
+	wait(&status);
+
+	return 0;
+}
