SimonKagstrom/kcov

commit b42703439673271b6aada6702576220e447d67a1

Release v27

src/reporter.cc
@@ -44,6 +44,7 @@ public:
 		m_collector.registerListener(*this);
 
 		m_hashFilename = fileParser.getParserType() == "ELF";
+		m_hashFilename = false;
 
 		m_dbFileName = IConfiguration::getInstance().keyAsString("target-directory") + "/coverage.db";
 	}

data/bcov.css
@@ -12,11 +12,14 @@ th.headerItem { text-align: right; padding-right: 6px; font-family: sans-serif; 
 th.headerValue { text-align: left; color: #284FA8; font-family: sans-serif; font-weight: bold; }
 pre.source { font-family: monospace; white-space: pre; overflow: hidden; text-overflow: ellipsis; }
 span.lineNum { background-color: #EFE383; }
+span.lineNumLegend { background-color: #EFE383; width: 96px; font-weight: bold ;}
 span.lineCov { background-color: #adff9a; }
 span.linePartCov { background-color: #fffe80; }
 span.lineNoCov { background-color: #ffbbbb; }
 span.orderNum { background-color: #e0b373; float: right; width:5em; text-align: left; }
+span.orderNumLegend { background-color: #e0b373; width: 96px; font-weight: bold ;}
 span.coverHits { background-color: #DFE383;  padding-left: 3px; padding-right: 1px;  text-align: right; list-style-type: none; display: inline-block; width: 5em; }
+span.coverHitsLegend { background-color: #DFE383; width: 96px; font-weight: bold; margin: 0 auto;}
 td.tableHead { text-align: center; color: #FFFFFF; background-color: #6688D4; font-family: sans-serif; font-size: 120%; font-weight: bold; }
 td.coverFile { text-align: left; padding-left: 10px; padding-right: 20px; color: #284FA8; font-family: monospace; }
 td.coverBar { padding-left: 10px; padding-right: 10px; background-color: #DAE7FE; }

data/source-file.html
@@ -18,29 +18,25 @@
       <table cellpadding="1" border="0" width="100%">
         <tr id="command">
           <td class="headerItem" width="20%">Command:</td>
-          <td id="header-command" class="headerValue" width="80%" colspan=6>???</td>
+          <td id="header-command" class="headerValue" width="60%" colspan=4>???</td>
+          <td><span class="lineNumLegend">Line number</span></td>
         </tr>
         <tr>
           <td class="headerItem" width="20%">Date: </td>
-          <td id="header-date" class="headerValue" width="15%"></td>
-          <td width="5%"></td>
+          <td id="header-date" class="headerValue" width="20%" colspan=2></td>
           <td class="headerItem" width="20%">Instrumented lines:</td>
-          <td id="header-instrumented" class="headerValue" width="10%">???</td>
+          <td id="header-instrumented" class="headerValue" width="20%">???</td>
+          <td><span class="coverHitsLegend">Hits</span></td>
         </tr>
         <tr>
           <td class="headerItem" width="20%">Code covered:</td>
-          <td id="header-percent-covered" width="15%">???</td>
-          <td width="5%"></td>
+          <td id="header-percent-covered" width="20%" colspan=2>???</td>
           <td class="headerItem" width="20%">Executed lines:</td>
-          <td id="header-covered" class="headerValue" width="10%">???</td>
+          <td id="header-covered" class="headerValue" width="20%">???</td>
+          <td><span class="orderNumLegend">Order</span></td>
         </tr>
       </table>
     </td>
-  </tr>
-  <tr>
-  <td>
-<b><source-line><span class="lineNum">Line#</span><span class="coverHits">Hits</span><span class="{{class}}"></span></span><span class="orderNum">Order</span></source-line></b>
-  </td>
   </tr>
   <tr><td class="ruler"><img src="data/glass.png" width="3" height="3" alt=""/></td></tr>
 </table>

commit 6ac673df3e141b28971f506d476de5b1b63ed229

Issue #101: reporter: Fix filename-hashing mistake introduced in b427034396

src/reporter.cc
@@ -44,7 +44,6 @@ public:
 		m_collector.registerListener(*this);
 
 		m_hashFilename = fileParser.getParserType() == "ELF";
-		m_hashFilename = false;
 
 		m_dbFileName = IConfiguration::getInstance().keyAsString("target-directory") + "/coverage.db";
 	}