5583d82eed5b74ada3e5e43b5b907c6645727b7f
Simon Kagstrom <simon.kagstrom@gmail.com>
2016-03-21 14:20:14 +0100

    lineid: static inline functions

--- src/include/lineid.hh
+++ src/include/lineid.hh
@@ -7,12 +7,12 @@
 
 namespace kcov
 {
-	static uint64_t getFileHash(const std::string &fileName)
+	static inline uint64_t getFileHash(const std::string &fileName)
 	{
 		return std::hash<std::string>()(fileName);
 	}
 
-	static uint64_t getLineId(const std::string &fileName, unsigned int nr)
+	static inline uint64_t getLineId(const std::string &fileName, unsigned int nr)
 	{
 		return (getFileHash(fileName) << 32) | ((uint64_t)nr);
 	}
