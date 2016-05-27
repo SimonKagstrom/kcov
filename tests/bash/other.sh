ee1007c59824ead67e16a10d341287692c782074
Simon Kagstrom <simon.kagstrom@gmail.com>
2016-05-27 19:07:19 +0200

    Issue #143: tests: Add regression test for for-loops

--- tests/bash/other.sh
+++ tests/bash/other.sh
@@ -6,3 +6,15 @@ if [ $1 -eq 5 ] ; then
 	echo "Kalle anka"
 fi
 
+for var in "$@" ; do
+	echo $var
+done
+
+round=0
+for (( ; ; )) do
+	round=$(($round + 1))
+	if [ $round -gt 10 ] ; then
+		break
+	fi
+	echo "hej $round"
+done
