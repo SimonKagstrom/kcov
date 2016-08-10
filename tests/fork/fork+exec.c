58079025d95bb57590dc07ab01b7ed8cc397c31a
Simon Kagstrom <simon.kagstrom@gmail.com>
2016-08-10 11:14:18 +0200

    Issue #149: tests: Add regression test for fork+exec of another binary

---
+++ tests/fork/fork+exec.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sched.h>
+
+int main(int argc, const char *argv[])
+{
+	pid_t child;
+	int status;
+
+	child = fork();
+	if (child < 0) {
+		fprintf(stderr, "fork failed!\n");
+		return -1;
+	}
+
+	if (child > 0) {
+		printf("Parent %d\n", getpid());
+	} else if (child < 0) {
+		printf("ERROR!\n");
+	} else {
+		printf("Child, will exec()\n");
+		execvp(argv[1], NULL);
+	}
+
+	waitpid(child, &status, 0);
+
+	return 0;
+}
