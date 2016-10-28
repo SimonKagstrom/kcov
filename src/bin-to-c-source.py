bcf10c42cd8a78480ba95c0a605bfcac92a7aedd
Simon Kagstrom <simon.kagstrom@gmail.com>
2016-10-28 12:51:28 +0200

    Issue #155: bin-to-source: Make compatible with both Python 2 and Python 3
    Use whatever is the system default

--- src/bin-to-c-source.py
+++ src/bin-to-c-source.py
@@ -1,37 +1,37 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+
+from __future__ import print_function
 
 import sys, struct
 
 def generate(data_in, base_name):
-	print "const uint8_t %s_data_raw[] = {" % (base_name)
+	print("const uint8_t %s_data_raw[] = {" % (base_name))
 
 	for i in range(0, len(data), 20):
-		line = data[i:i+20]
+		line = bytearray(data[i:i+20])
 
 		for val in line:
-			v = struct.unpack(">B", val)
+			print("0x%02x," % (val), end="")
+		print("")
 
-			print "0x%02x," % (v),
-		print ""
-
-	print "};"
-	print "GeneratedData %s_data(%s_data_raw, sizeof(%s_data_raw));" % (base_name, base_name, base_name)
+	print("};")
+	print("GeneratedData %s_data(%s_data_raw, sizeof(%s_data_raw));" % (base_name, base_name, base_name))
 
 if __name__ == "__main__":
 	if len(sys.argv) < 3 or (len(sys.argv) - 1) % 2 != 0:
-		print "Usage: bin-to-source.py <file> <base-name> [<file2> <base-name2>]"
+		print("Usage: bin-to-source.py <file> <base-name> [<file2> <base-name2>]")
 		sys.exit(1)
 
-	print "#include <stdint.h>"
-	print "#include <stdlib.h>"
-	print "#include <generated-data-base.hh>"
-	print "using namespace kcov;"
+	print("#include <stdint.h>")
+	print("#include <stdlib.h>")
+	print("#include <generated-data-base.hh>")
+	print("using namespace kcov;")
 
 	for i in range(1,len(sys.argv), 2):
 		file = sys.argv[i]
 		base_name = sys.argv[i + 1]
 
-		f = open(file)
+		f = open(file, "rb")
 		data = f.read()
 		f.close()
 
