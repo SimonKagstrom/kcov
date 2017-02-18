be913456b9cad59324d2f42ad2f44f687a84e619
Bashkar Rana <email>
2017-02-18 12:30:30 +0100

    Issue #183: python-engine: Quote argument strings to avoid argv issues

--- src/engines/python-engine.cc
+++ src/engines/python-engine.cc
@@ -93,7 +93,7 @@ public:
 					conf.keyAsString("python-command").c_str(),
 					kcov_python_path.c_str());
 			for (unsigned int i = 0; i < argc; i++)
-				s += std::string(argv[i]) + " ";
+				s += "'" + std::string(argv[i]) + "' ";
 
 			int res;
 
