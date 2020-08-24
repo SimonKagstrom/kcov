c481faae049e9cdc4365c355874a15ee3826a0fe
Tom Shaver <l3l_aze@yahoo.com>
2020-08-23 22:49:03 -0500

    Add automated build + release for tags via GitHub Actions.

---
+++ .github/workflows/setup-i386.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install schroot debootstrap
+install -d /chroots/i386
+
+printf "[mychroot]
+type=directory
+description=An i386 chroot for building kcov.
+directory=/chroots/i386
+users=$(whoami)
+root-groups=root
+preserve-environment=true
+personality=linux32" >> /etc/schroot/schroot.conf
+
+debootstrap --variant=buildd --arch i386 bionic /chroots/i386 http://archive.ubuntu.com/ubuntu/
