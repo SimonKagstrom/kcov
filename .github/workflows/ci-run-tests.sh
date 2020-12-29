14315cb9e1990556d37e5fb26db4e9fd753af944
Simon Kagstrom <simon.kagstrom@gmail.com>
2020-12-29 13:02:48 +0100

    github actions: Run the test suite

--- .github/workflows/ci-run-tests.sh
+++ .github/workflows/ci-run-tests.sh
@@ -3,17 +3,7 @@
 set -euo pipefail
 
 run () {
-  export PATH="${PATH}:${HOME}/kcov/bin"
-
-  chmod u+x .github/workflows/test-executable.sh
-  kcov --include-path=.github/workflows/test-executable.sh coverage .github/workflows/test-executable.sh
-
-  local coverage="$(<coverage/test*.sh*/coverage.json)"
-  local percent="${coverage##*percent_covered\": \"}"
-  local total_lines="${coverage##*total_lines\": }"
-  local covered_lines="${coverage##*covered_lines\": }"
-
-  echo -e "Coverage: ${covered_lines%%,*}/${total_lines%%,*} ${percent%%\"*}%"
+  tests/tools/run-tests build/src/kcov /tmp/ build-tests/ `pwd` -v || exit 64
 }
 
 run "$@"
