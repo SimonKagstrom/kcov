SimonKagstrom/kcov

commit 720eb98b898db31ee9124932aa21d45ab6ba4c48

configuration: refactorize code scanning for executables
Add the look_path function for searching executables in directories
named by the PATH environment variable in the utils source file.

Refactorize the parse function in configuration.cc, using the new
look_path function.  This greatly simplify the code.

src/include/utils.hh
@@ -223,3 +223,7 @@ static inline uint32_t hash_block(const void *buf, size_t len)
 }
 
 uint32_t hash_file(const std::string &filename);
+
+// Searches for an executable named file in the directories named by the PATH
+// environment variable.
+int look_path(const std::string &file, std::string *out);

src/utils.cc
@@ -775,3 +775,36 @@ uint32_t hash_file(const std::string &filename)
 
 	return out;
 }
+
+int look_path(const std::string &file, std::string *out)
+{
+	const char *sys_path = getenv("PATH");
+	if (!sys_path)
+		return -1;
+
+	const std::vector<std::string> sys_paths = split_string(sys_path, ":");
+	for (std::vector<std::string>::const_iterator it = sys_paths.begin();
+			it != sys_paths.end(); ++it)
+	{
+		const std::string root = *it;
+		const std::string path = get_real_path(root + "/" + file);
+		struct stat st;
+
+		if (lstat(path.c_str(), &st) < 0)
+			continue;
+
+		// Regular file?
+		if (S_ISREG(st.st_mode) == 0)
+			continue;
+
+		// Executable?
+		if ((st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) == 0)
+			continue;
+
+		*out = path;
+
+		return 0;
+	}
+
+	return -1;
+}

src/configuration.cc
@@ -13,9 +13,18 @@
 #include <stdexcept>
 #include <unordered_map>
 #include <iostream>
+
 using namespace kcov;
 
 extern "C" const char *kcov_version;
+
+// This function can't be defined in utils.cc due to the use of the singleton
+// pattern. It will cause a linker error when buildind tests, since the utils.cc
+// file is also included in the test source files.
+static bool is_supported_executable(const std::string path)
+{
+	return kcov::IParserManager::getInstance().matchParser(path) != 0;
+}
 
 class Configuration : public IConfiguration
 {
@@ -164,56 +173,33 @@ public:
 
 		setupDefaults();
 
-		const char *path = getenv("PATH");
-
-		if (!path)
-			path = "";
-
-		std::vector<std::string> paths = split_string(path, ":");
-
 		setKey("kcov-binary-path", get_real_path(argv[0]));
 
-		/* Scan through the parameters for an ELF file: That will be the
-		 * second last argument in the list.
+		/* Scan through the parameters for a supported executable file: That
+		 * will be the second last argument in the list.
 		 *
 		 * After that it's arguments to the external program.
 		 */
 		for (lastArg = 1; lastArg < argc; lastArg++)
 		{
-			if (IParserManager::getInstance().matchParser(get_real_path(argv[lastArg])))
-				break;
+			std::string arg = std::string(argv[lastArg]);
+			int ret;
+			std::string path;
 
-			bool found = false;
-			for (std::vector<std::string>::const_iterator it = paths.begin();
-					it != paths.end(); ++it)
+			// Skip options.
+			if ((arg.size() > 0) && (arg[0] == '-'))
+				continue;
+
+			ret = look_path(arg, &path);
+			if (ret < 0)
+				continue;
+
+			if (is_supported_executable(path))
 			{
-				const std::string &curPath = *it;
-				const std::string cur = get_real_path(
-						curPath + "/" + argv[lastArg]);
-				struct stat st;
-
-				if (lstat(cur.c_str(), &st) < 0)
-					continue;
-
-				// Regular file?
-				if (S_ISREG(st.st_mode) == 0)
-					continue;
-
-				// Executable?
-				if ((st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) == 0)
-					continue;
-
-				if (IParserManager::getInstance().matchParser(cur))
-				{
-					// Intentional memory leak
-					argv[lastArg] = xstrdup(cur.c_str());
-					found = true;
-					break;
-				}
-			}
-
-			if (found)
+				// Intentional memory leak
+				argv[lastArg] = xstrdup(path.c_str());
 				break;
+			}
 		}
 
 		bool printUsage = false;

commit b587483cc02ce0cef4882ed37bb89699f061ddbf

configuration: fix handling relative executables
Commit 720eb98 (configuration: refactorize code scanning for
executables) introduced a bug when the executable is a relative path.
In this case the file is always expanded, thus causing file to be always
considered non executable by the code scanning through the parameters
in configuration.cc.

Update the look_path function to first try a file without consulting
the PATH environment variable.

Add the find_executable function, and update the code to check the file
is not a directory instead of checking if it is a regular file, so that
symlinks are handled correctly.

Update the peek_file function to do the same, so that ParseManager can
handle symlinks correctly.

The old code worked because the last argument (before the executable
arguments) was update correctly thanks to the loop over the PATH
environment variable.

Note that the correct solution is to add "--" before the executable
arguments, but this will break the kcov cli interface.

src/include/utils.hh
@@ -224,6 +224,12 @@ static inline uint32_t hash_block(const void *buf, size_t len)
 
 uint32_t hash_file(const std::string &filename);
 
+int find_executable(const std::string &file);
+
 // Searches for an executable named file in the directories named by the PATH
 // environment variable.
+// The named file is first tried directly without consulting PATH.
+//
+// Don't use this function when expanding the first argument of the exec()
+// family of functions, since it is insecure.
 int look_path(const std::string &file, std::string *out);

src/utils.cc
@@ -146,8 +146,9 @@ void *peek_file(size_t *out_size, const char *fmt, ...)
 	struct stat st;
 	if (lstat(path, &st) < 0)
 		return NULL;
-	// Regular file?
-	if (S_ISREG(st.st_mode) == 0)
+
+	// Not a directory?
+	if (S_ISDIR(st.st_mode) > 0)
 		return NULL;
 
 	// Read a little bit of the file    
@@ -776,8 +777,35 @@ uint32_t hash_file(const std::string &filename)
 	return out;
 }
 
+int find_executable(const std::string &file)
+{
+	struct stat st;
+
+	if (lstat(file.c_str(), &st) < 0)
+		return -1;
+
+	// Not a directory?
+	if (S_ISDIR(st.st_mode) > 0)
+		return -1;
+
+	// Executable?
+	if ((st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) == 0)
+		return -1;
+
+	return 0;
+}
+
+
 int look_path(const std::string &file, std::string *out)
 {
+	// First check if the relative file is an executable.
+	if (find_executable(file) == 0)
+	{
+		*out = file;
+
+		return 0;
+	}
+
 	const char *sys_path = getenv("PATH");
 	if (!sys_path)
 		return -1;
@@ -788,22 +816,13 @@ int look_path(const std::string &file, std::string *out)
 	{
 		const std::string root = *it;
 		const std::string path = get_real_path(root + "/" + file);
-		struct stat st;
 
-		if (lstat(path.c_str(), &st) < 0)
-			continue;
+		if (find_executable(path) == 0)
+		{
+			*out = path;
 
-		// Regular file?
-		if (S_ISREG(st.st_mode) == 0)
-			continue;
-
-		// Executable?
-		if ((st.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH)) == 0)
-			continue;
-
-		*out = path;
-
-		return 0;
+			return 0;
+		}
 	}
 
 	return -1;