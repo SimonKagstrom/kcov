1bd0b15fd139e302dc25bed2f352b74ad6ec3c63
Manlio Perillo <manlio.perillo@gmail.com>
2024-04-14 18:43:23 +0200

    ci: use python3, instead of python in ci-run-tests.sh
    Using python3 will avoid unspecified behavior when using Python on
    different systems, causing an error when trying to run the tests.
    
    See https://peps.python.org/pep-0394/

--- .github/workflows/ci-run-tests.sh
+++ .github/workflows/ci-run-tests.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 run () {
   export PYTHONPATH=tests/tools
-  python -m libkcov build/src/kcov /tmp/ build-tests/ "." -v "$@" || exit 64
+  python3 -m libkcov build/src/kcov /tmp/ build-tests/ "." -v "$@" || exit 64
 }
 
 run "$@"
